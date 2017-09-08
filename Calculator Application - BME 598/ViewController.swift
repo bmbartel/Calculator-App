b8f98a8ea57c2d70f20a60ac5c5d5a7e26969680
Brandon <bmbartel@asu.edu>
2017-09-08 14:33:15 -0700

    Display changes when buttons pressed

--- Calculator Application - BME 598/ViewController.swift
+++ Calculator Application - BME 598/ViewController.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // References to the Buttons and Label
+    @IBOutlet weak var One: UIButton!
+    @IBOutlet weak var Two: UIButton!
+    @IBOutlet weak var Three: UIButton!
+    @IBOutlet weak var Four: UIButton!
+    @IBOutlet weak var Five: UIButton!
+    @IBOutlet weak var six: UIButton!
+    @IBOutlet weak var Seven: UIButton!
+    @IBOutlet weak var Eight: UIButton!
+    @IBOutlet weak var Nine: UIButton!
+    @IBOutlet weak var Zero: UIButton!
+    @IBOutlet weak var NumericDisplay: UILabel!
+    
+    // If button is pressed, chance numeric display to that value.
+    @IBAction func ButtonOnePressed(_ sender: Any) {
+        NumericDisplay.text = "1"
+    }
+    
+    @IBAction func ButtonTwoPressed(_ sender: Any) {
+        NumericDisplay.text = "2"
+    }
+    @IBAction func ButtonThreePressed(_ sender: Any) {
+        NumericDisplay.text = "3"
+    }
+    @IBAction func ButtonFourPressed(_ sender: Any) {
+        NumericDisplay.text = "4"
+    }
+    @IBAction func ButtonFivePressed(_ sender: Any) {
+        NumericDisplay.text = "5"
+    }
+    @IBAction func ButtonSixPressed(_ sender: Any) {
+        NumericDisplay.text = "6"
+    }
+    @IBAction func ButtonSevenPressed(_ sender: Any) {
+        NumericDisplay.text = "7"
+    }
+    
+    @IBAction func ButtonEightPressed(_ sender: Any) {
+        NumericDisplay.text = "8"
+    }
+    @IBAction func ButtonNinePressed(_ sender: Any) {
+        NumericDisplay.text = "9"
+    }
+    @IBAction func ButtonZeroPressed(_ sender: Any) {
+        NumericDisplay.text = "0"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
